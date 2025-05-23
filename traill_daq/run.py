c74917eca6a40a85efceda41efe6a72006f59bee
yihan-liu <y1hliu@unc.edu>
2025-05-23 12:46:14 -0400

    enables profile selection

--- traill_daq/run.py
+++ traill_daq/run.py
@@ -1,4 +1,8 @@
-from traill_daq.traill_visualizer import TRAiLLVisualizer
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from traill_visualizer import TRAiLLVisualizer
 import argparse
 
 if __name__ == '__main__':
@@ -6,11 +10,19 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=str, default='COM31', help='Port to connect to TRAiLL.')
     parser.add_argument('--path', type=str, default=None, help='Path to save data.')
     parser.add_argument('--disable-csv', action='store_true', help='Disable csv file saving.')
-    
+    parser.add_argument('--profile', type=str, default=None, help='Name of the activity profile to use (from activity_profiles.json).')
+
     args = parser.parse_args()
     serial_port = args.port
     path = args.path
     disable_csv = args.disable_csv
-    
-    visualizer = TRAiLLVisualizer(serial_port=serial_port, data_folder=path, disable_csv=disable_csv)
+    profile = args.profile
+
+    visualizer = TRAiLLVisualizer(
+        serial_port=serial_port,
+        data_folder=path,
+        disable_csv=disable_csv,
+        profile_name=profile,
+        profile_json_path='activity_profiles.json'
+    )
     visualizer.run()
